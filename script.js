c1fdfacef4a788d710385fac435adcc478dace7f
c0439461-hub <c0439461@gmail.com>
2026-06-29 11:53:29 +0300

    finish assigment

---
+++ script.js
@@ -0,0 +1,4 @@
+// Set page background color via JavaScript to avoid CSS in a .js file
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.style.backgroundColor = 'white';
+});
